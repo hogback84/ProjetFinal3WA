d75ec119b07211f261d6f719d193c4d82724a891
mohamed <nuovocoso@gmail.com>
2023-06-17 00:12:44 +0200

    update

--- api/db/connection.js
+++ api/db/connection.js
@@ -3,11 +3,12 @@ import { promisify } from "util";
 
 export const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "mohamed",
-  password: "Nuovocoso-1",
-  database: "booktest",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 export const getConnection = () => {
   return {
     query: promisify(pool.query).bind(pool),
